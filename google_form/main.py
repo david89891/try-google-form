07eca6d35f7cc4a5eff3e3e3342ce42363348db0
david89891 <david89891@gmail.com>
2022-02-20 14:43:25 +0800

    Add files via upload

---
+++ google_form/main.py
@@ -0,0 +1,31 @@
+import json
+import re
+import requests
+import time
+
+from msedge.selenium_tools import Edge, EdgeOptions
+
+import lib
+
+
+opt = EdgeOptions()
+opt.use_chromium = True
+opt.add_argument('inprivate')
+
+url = input()
+url_request = requests.get(url)
+url = url_request.url
+
+html_data = requests.get(url).text
+data = json.loads(re.search(r"FB_PUBLIC_LOAD_DATA_ = (.*?);", html_data, flags=re.S).group(1))
+
+lib.data = data[1][1]
+url += lib.combine_url(url)
+
+driver = Edge(executable_path='driver-for-selenium/msedgedriver.exe', options=opt)
+driver.maximize_window()
+driver.get(url)
+time.sleep(100)
+
+# https://docs.google.com/forms/d/e/1FAIpQLSfwiZ7BzQLRJoFFj7NQiXu2Gw_ToKwX2jQsuSYrGGZTlO7I4g/viewform?usp=sf_link
+# https://forms.gle/PdaMu3drsdcZ2MjQ7
